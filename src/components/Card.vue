e4901cc07aaf200200d8eb2a5ef50495517b0c7b
arneesh <arneeshaima@gmail.com>
2021-02-28 02:59:07 +0530

    created a basic view using vue bootstrap row col card

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-card
+      :title="user.name"
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Job Info"
+      img-top
+      img-tag="article"
+      style="max-width:20rem"
+      footer="Card Footer"
+    >
+      <b-card-text>
+        <b-card> Email: {{ user.email }} </b-card>
+      </b-card-text>
+      <b-button variant="primary"> Apply </b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <b-container align-v="center">
+    <b-row>
+      <b-col
+        md="4"
+        class="mb-3"
+        v-for="(user, index) in users"
+        v-bind:key="index"
+      >
+        <card :user="user" />
+      </b-col>
+    </b-row>
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+    ></b-pagination>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Card from "@/components/Card.vue";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      users: [],
+      currentPage: 1,
+      rows: 1,
+      perPage: 3
+    };
+  },
   components: {
-    HelloWorld
+    card: Card
+  },
+  methods: {
+    fetchData: async function() {
+      const res = await fetch("names.json");
+      const dataList = await res.json();
+      this.users = dataList;
+      this.rows = this.users.length;
+    }
+  },
+  mounted() {
+    this.fetchData();
   }
 };
 </script>
