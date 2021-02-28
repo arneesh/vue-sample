d63fdc11ce335158383565bd08651ea912761b69
arneesh <arneeshaima@gmail.com>
2021-02-28 08:00:31 +0530

    added basic state and mutations

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,20 +7,51 @@
       img-top
       img-tag="article"
       style="max-width:20rem"
-      footer="Card Footer"
+      footer="Foter"
     >
       <b-card-text>
-        <b-card> Email: {{ user.email }} </b-card>
+        <b-card>
+          Email: {{ user.email }} <br />
+          <b> {{ count }}</b>
+        </b-card>
       </b-card-text>
-      <b-button variant="primary"> Apply </b-button>
+      <b-button class="mb-3" variant="primary" @click="increment(10)"
+        ><b>Increment</b>
+      </b-button>
+
+      <b-form-rating
+        v-model="rating_value"
+        variant="warning"
+        class="mb-2"
+      ></b-form-rating>
     </b-card>
   </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from "vuex";
+
 export default {
   props: {
     user: Object
+  },
+  data() {
+    return {
+      rating_value: 1
+    };
+  },
+  computed: {
+    ...mapState({
+      count: state => state.count
+    }),
+    ...mapGetters({})
+  },
+  methods: {
+    increment: function(value) {
+      this.$store.commit("increment", {
+        amount: value
+      });
+    }
   }
 };
 </script>
